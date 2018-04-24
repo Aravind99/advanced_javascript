6a937a9c1abb30b4ed356feb55e0c9ed8245fee2
ARAVIND99 <aravind@classpro.in>
2018-04-24 18:29:44 +0530

    * Scope and The Compiler

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# advanced_javascript
-Advanced Javascript Learning Material
+# Advanced
+
+The path to learn advanced javascript.
+If you find it helpful, :star: star the repo as appreciation 
+
+## 1.Scope
+    1. [Scope and The javascript compiler.](https://www.classpro.in) 
---
+++ chapter-1/scope_and_the_javascript_compiler.js
@@ -0,0 +1,43 @@
+/* Example 1 */
+var foo = "bar";
+
+function bar() {
+    var foo = "baz";
+}
+
+function baz(foo) {
+    foo = "bam";
+    bam = "yay";
+}
+
+/* Example 2 */
+var foo = "bar";
+
+function bar() {
+    var foo = "baz";
+
+    function baz(foo) {
+        foo = "bam";
+        bam = "yay";
+    }
+    baz();
+}
+
+bar();
+foo;    // "bar"
+bam;    // "yay"
+baz();  // "Error!" ReferenceError
+
+/* Example 3 */
+var foo = function bar() {
+    var foo = "baz";
+
+    function baz(foo) {
+        foo = bar;
+        foo;
+    }
+    baz();
+};
+
+foo();
+bar(); // Error! ReferenceError
