476f2dd3d3ea10b939915709c9d7f8b9a27f7c62
Aravind Vemula <vemula.aravind336@gmail.com>
2018-04-26 08:18:14 -0700

    Update closures.js

--- chapter-2/closures.js
+++ chapter-2/closures.js
@@ -1,1 +1,58 @@
+/*
+  Closure is when a function 
+  "remembers" its lexical scope
+  even when the function is 
+  executed outside that lexical 
+  scope.
+*/
 
+/* Closure */
+/* Example 1 */
+function foo() {
+	var bar = "bar";
+
+	function baz() {
+		console.log(bar);
+	}
+
+	bam(baz);
+}
+
+function bam(baz) {
+	baz();            // "bar"
+}
+
+foo();
+// ----------------------------
+
+/* Example 2 */
+function foo() {
+	var bar = "bar";
+
+	return function() {
+		console.log(bar);
+    };
+}
+
+function bam() {
+	foo()();        // "bar"
+}
+
+bam();
+// -----------------------------
+
+/* Example 3 */
+function foo() {
+	var bar = "bar";
+
+	setTimeout(function() {
+		console.log(bar);
+    }, 1000);
+}
+
+foo(); 
+// -----------------------------
+
+/* Example 4 */
+
+// -----------------------------
